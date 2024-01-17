3b54c36b6b982c0eff7ede4f8a78badb9c37dea6
Igor Guryan <sacujo93@yandex.ru>
2024-01-17 03:53:43 +0300

    refactor + add checker

--- Today/ListViewController/ReminderListViewController+DataSource.swift
+++ Today/ListViewController/ReminderListViewController+DataSource.swift
@@ -13,11 +13,32 @@ extension ReminderListViewController {
     
     
     func cellRegistrationHandler(cell: UICollectionViewListCell, indexPath: IndexPath, item: Reminder) {
-            var content = cell.defaultContentConfiguration()
-            content.text = item.title
-            content.secondaryText = item.dueDate.dayAndTimeText
+        var content = cell.defaultContentConfiguration()
+        content.text = item.title
+        content.secondaryText = item.dueDate.dayAndTimeText
         content.secondaryTextProperties.font = UIFont.preferredFont(forTextStyle: .caption1)
-            cell.contentConfiguration = content
-            
+        cell.contentConfiguration = content
+        
+        
+        var doneButtonConfiguration = doneButtonConfiguration(for: item)
+        doneButtonConfiguration.tintColor = .todayListCellDoneButtonTint
+        cell.accessories = [.customView(configuration: doneButtonConfiguration), .disclosureIndicator(displayed: .always)]
+        
+        var backgroundConfiguration = UIBackgroundConfiguration.listGroupedCell()
+        backgroundConfiguration.backgroundColor = .todayListCellBackground
+        cell.backgroundConfiguration = backgroundConfiguration
         }
+    
+    private func doneButtonConfiguration(for reminder: Reminder) -> UICellAccessory.CustomViewConfiguration {
+        let symbolName = reminder.isComplete ? "checkmark.circle" : "circle"
+        let sybmolConfiguration = UIImage.SymbolConfiguration(textStyle: .title1)
+        let image = UIImage(systemName: symbolName, withConfiguration: sybmolConfiguration)
+        let button = UIButton()
+        button.setImage(image, for: .normal)
+        return UICellAccessory.CustomViewConfiguration(customView: button, placement: .leading(displayed: .always))
     }
+    
+    }
+
+
+    
